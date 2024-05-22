72c5caa65af0c0202092e1965cad8fc88a13424b
François Rice <francoisrice@gmail.com>
2024-05-21 20:30:05 -0400

    Scaffold core logic

--- price-predictor.py
+++ price-predictor.py
@@ -1,7 +1,81 @@
-import openbb as bb
+from openbb import obb
+import numpy as np
+import os
+
+
+class PricePredictor:
+    # Note: Fetched stock data has a daily interval
+
+    obb.account.login(pat=os.environ["OPENBB_KEY"])
+
+    def __init__(self, symbol="") -> None:
+        self.symbol = symbol
+
+    def fetch_stock_data(self, symbol="") -> None:
+        stockDataFrame = obb.equity.price.historical(
+            symbol,
+            start_date=None,
+            interval="1d",
+            include_actions=False,
+            provider="yfinance",
+        ).to_df()
+
+        # Calc Median prices for the each timeframe
+        # avgPrice = (high + low + close) / 3
+
+        # self.stockData = [timestamps,avgPrices]
+
+    # 1.
+    def calculate_volatility(self) -> None:
+        # Daily volatility over the last 21 trading days, annualized
+        # self.stockData
+
+        returns = [
+            np.log(avgPrices[index + 1] / avgPrices[index])
+            for index in range(len(avgPrices) - 1)
+        ]
+
+        stdDev = np.std(returns)
+
+        self.volatility = stdDev * np.sqrt(252)
+
+    def volatilityIsNone(self):
+        return self.volatility == None
+
+    # 2.
+    def calculate_drift(self):
+
+        logRate = 0
+        # logRate = (np.log(data[-1]) - np.log(data[0])) / timePeriod
+        # logRate = (np.log(stockData[-1]) - np.log(stockData[0])) / (timestamp[-1] - timestamp[0])
+
+        if self.volatilityIsNone():
+            self.calculate_volatility()
+
+        self.drift = self.volatility**2 / 2 + logRate
+
+        # Add Interest rate
+        # += fetch_interest_rate()
+
+
+# 10.  def fetch_interest_rate(): # Needed?
+#     return None
+
+# 9. def fetch_current_price():
+#     return None
 
 
 def main():
+    currentPrice = 10
+
+    pp = PricePredictor("MARA")
+
+    # calculate_mean_price()
+    # calculate_drift()
+    # calculate_volatility() -> std_dev
+
+    # Probability is probability based on std dev input price is away from mean price, factoring in drift
+
     print("Hello World!")
 
 
