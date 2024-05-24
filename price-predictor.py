592a3bdb9efa8ade7b06f3f052cd13e2aa968585
François Rice <francoisrice@gmail.com>
2024-05-24 17:32:31 -0400

    Pull data, Calc price, volatility, & drift

--- price-predictor.py
+++ price-predictor.py
@@ -1,6 +1,10 @@
+import sys
 from openbb import obb
 import numpy as np
 import os
+
+# Cache stock data for a day
+# Add pytest tests
 
 
 class PricePredictor:
@@ -12,8 +16,10 @@ class PricePredictor:
         self.symbol = symbol
 
     def fetch_stock_data(self, symbol="") -> None:
+        if symbol:
+            self.symbol = symbol
         stockDataFrame = obb.equity.price.historical(
-            symbol,
+            symbol=self.symbol,
             start_date=None,
             interval="1d",
             include_actions=False,
@@ -21,18 +27,27 @@ class PricePredictor:
         ).to_df()
 
         # Calc Median prices for the each timeframe
-        # avgPrice = (high + low + close) / 3
+        stockDataFrame["avgPrice"] = (
+            stockDataFrame["high"] + stockDataFrame["low"] + stockDataFrame["close"]
+        ) / 3
 
-        # self.stockData = [timestamps,avgPrices]
+        self.stockData = stockDataFrame["avgPrice"]
+        self.currentPrice = stockDataFrame["close"].iloc[-1]
 
-    # 1.
     def calculate_volatility(self) -> None:
         # Daily volatility over the last 21 trading days, annualized
-        # self.stockData
+
+        # prices = self.stockData["avgPrice"]
+        prices = self.stockData.to_list()
+
+        if not prices:
+            self.fetch_stock_data()
+            # prices = self.stockData["avgPrice"]
+            prices = self.stockData.to_list()
 
         returns = [
-            np.log(avgPrices[index + 1] / avgPrices[index])
-            for index in range(len(avgPrices) - 1)
+            np.log(prices[index + 1] / prices[index])
+            for index in range(len(prices) - 1)
         ]
 
         stdDev = np.std(returns)
@@ -42,42 +57,71 @@ class PricePredictor:
     def volatilityIsNone(self):
         return self.volatility == None
 
-    # 2.
     def calculate_drift(self):
+        prices = self.stockData.to_list()
 
-        logRate = 0
-        # logRate = (np.log(data[-1]) - np.log(data[0])) / timePeriod
-        # logRate = (np.log(stockData[-1]) - np.log(stockData[0])) / (timestamp[-1] - timestamp[0])
+        if prices:
+            if len(prices) > 21:
+                logRate = (
+                    np.log(prices[-1] / prices[-21])
+                ) / 21  # price change in the last month
+            else:
+                logRate = (np.log(prices[-1] / prices[0])) / len(
+                    prices
+                )  # price change over the available data
 
-        if self.volatilityIsNone():
-            self.calculate_volatility()
+            if self.volatilityIsNone():
+                self.calculate_volatility()
 
-        self.drift = self.volatility**2 / 2 + logRate
+            self.drift = self.volatility**2 / 2 + logRate
 
-        # Add Interest rate
-        # += fetch_interest_rate()
+            # Add Interest rate
+            # += fetch_interest_rate()
 
 
 # 10.  def fetch_interest_rate(): # Needed?
 #     return None
 
-# 9. def fetch_current_price():
-#     return None
 
+def main(args):
+    print(args)
+    symbol = args[1]
+    if float(args[2]) > 0:
+        inputPrice = float(args[2])
+    else:
+        probability = float(args[2])
 
-def main():
-    currentPrice = 10
+    # pp = PricePredictor("MARA")
+    pp = PricePredictor(symbol)
 
-    pp = PricePredictor("MARA")
+    pp.fetch_stock_data()
+    pp.calculate_volatility()
+    pp.calculate_drift()
+
+    interestRate = 0.05
+    # interestRate = fetch_interest_rate()
 
     # calculate_mean_price()
     # calculate_drift()
     # calculate_volatility() -> std_dev
 
     # Probability is probability based on std dev input price is away from mean price, factoring in drift
-
-    print("Hello World!")
+    if inputPrice:
+        # Calculate probability of price being above inputPrice
+        zScore = (inputPrice - pp.currentPrice) / pp.volatility
+        probability = 1 - np.exp(-zScore)
+        print(probability)
+    elif probability:
+        # Calculate prices based on probability
+        zScore = -np.log(1 - probability)
+        inputPrice = pp.currentPrice + zScore * pp.volatility
+        print(inputPrice)
+    else:
+        print("Must provide a price or a percent probability.")
 
 
 if __name__ == "__main__":
-    main()
+    """
+    Usage: python price-predictor.py <symbol> <price|probability>
+    """
+    main(sys.argv)
